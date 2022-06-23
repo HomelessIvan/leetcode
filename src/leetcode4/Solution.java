981d142191346de8170ef97be14010447aa6b2b1
Ivan Bezdomniy <ivanbezdomniyworking@gmail.com>
2022-06-23 20:27:39 +0300

    leetcode4 median of 2 sorted arrays
    i realise that in task bigO should be log(n+m), but in standard sort it is n(log(n+m))
    so this is algo for merge sorted arrays

--- src/leetcode4/Solution.java
+++ src/leetcode4/Solution.java
@@ -5,13 +5,50 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class Solution {
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int[] nums = Stream.of(nums1, nums2).flatMapToInt(Arrays::stream).sorted().toArray();
-        boolean isLengthEven = nums.length % 2 == 0;
+        int[] result = mergeSorted(nums1, nums2);
+
+
+        boolean isLengthEven = result.length % 2 == 0;
         if (!isLengthEven)
-            return nums[nums.length / 2 - 1];
+            return result[result.length / 2];
         else
-            return (double) (nums[nums.length / 2 - 1] + nums[nums.length / 2 ])/2;
+            return (double) (result[result.length / 2 - 1] + result[result.length / 2]) / 2;
     }
+
+    private static int[] mergeSorted(int[] nums1, int[] nums2) {
+        if (nums1.length==0)
+            return nums2;
+        if (nums2.length==0)
+            return nums1;
+        int[] result = new int[nums1.length + nums2.length];
+        int i = 0;
+        int index1 = 0;
+        int index2 = 0;
+        while (i < result.length) {
+
+            if (nums1[index1] < nums2[index2]) {
+                result[i] = nums1[index1];
+                index1++;
+            } else {
+                result[i] = nums2[index2];
+                index2++;
+            }
+
+            if (index1 == nums1.length){
+                System.arraycopy(nums2, index2, result, i+1, nums2.length - index2);
+                break;
+            }
+
+            if (index2 == nums2.length){
+                System.arraycopy(nums1, index1, result, i+1, nums1.length - index1);
+                break;
+            }
+            i++;
+        }
+        return result;
+    }
+
 }
